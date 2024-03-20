12597a4d3642aa38f0a5cbeb380e473c878e1c16
MitrichMan <122974224+MitrichMan@users.noreply.github.com>
2024-03-20 09:19:23 -0300

    Initial Commit

---
+++ JobHunterTestApp/JobHunterTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobHunterTestAppApp.swift
+//  JobHunterTestApp
+//
+//  Created by Dmitrii Melnikov on 20.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct JobHunterTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
